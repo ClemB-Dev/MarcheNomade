58ae43cd953e80f8d61720f1286cf10497150fb7
Clémence <clemence.blachon@grafenit.com>
2022-09-07 20:19:19 +0200

    authentification configuration

---
+++ backend/marche_nomade/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import MyTokenObtainPairView, get_routes
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
+urlpatterns = [
+    path('', get_routes),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
