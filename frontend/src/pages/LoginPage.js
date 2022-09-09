2f6fddfdd1c3a9ad1662a49647a267528b29ec57
Clémence <clemence.blachon@grafenit.com>
2022-09-09 17:42:53 +0200

    styling

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,24 +1,43 @@
 import React, {useContext} from 'react'
 import AuthContext from '../context/AuthContext'
 import {Link} from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import '../css/signIn.css'
+
 
 const LoginPage = () => {
   let {loginUser} = useContext(AuthContext)
   return (
-    <div>
-      <form onSubmit={loginUser}>
-        <input
-            type='text'
-            name='username'
-            placeholder='Enter username'/>
-        <input
-            type='password'
-            name='password'
-            placeholder='Enter password'/>
-        <input
-            type='submit'/>
-      </form>
-      <Link to='/register'>Register</Link>
+    <div className='container'>
+      <div className='screen'>
+        <div className='loginContainer'>
+          <form className='login' onSubmit={loginUser}>
+            <div className='login_field'>
+              <label>Username</label><br></br>
+              <input
+                  className='login_input'
+                  type='text'
+                  name='username'
+                  placeholder='Enter username'/>
+            </div>
+            <div className='login_field'>
+              {/* <i className="login_icon fas fa-lock"></i> */}
+              <label>Password</label><br></br>
+              <input
+                  className='login_input'
+                  type='password'
+                  name='password'
+                  placeholder='Enter password'/>
+            </div>
+            <div className='login_submit'>
+              <input
+                  className='button_text'
+                  type='submit'/>
+            </div>
+          </form>
+          <Link className='register-btn' to='/register'>Register</Link>
+        </div>
+      </div>
     </div>
   )
 }
