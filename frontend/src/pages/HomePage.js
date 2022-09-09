1bc82e005981e5ea841e0863b0d5a1df3e53863d
Clémence <clemence.blachon@grafenit.com>
2022-09-09 17:42:08 +0200

    Mapbox library + database data retrieved

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import axios from 'axios'
+import React, {useState, useEffect, useContext} from 'react'
+import AuthContext from '../context/AuthContext'
+import useAxios from '../utils/useAxios'
+import ReactMapGl from 'react-map-gl'
+import 'mapbox-gl/dist/mapbox-gl.css';
+import Map from '../components/Map'
 
 const HomePage = () => {
+  let [market, setMarket] = useState([])
+  let {authTokens, logoutUser} = useContext(AuthContext)
+
+  // let api = useAxios()
+
+
+  useEffect(() => {
+    getMarkets()
+  }, [])
+
+  let getMarkets = async() => {
+    let response = await axios.get('http://127.0.0.1:8000/marche_nomade/markets/')
+
+    if(response.status === 200){
+      setMarket(response.data)
+    }
+}
+
   return (
     <div>
-      <p>You are logged into the Home Page</p>
+      <p>Markets</p>
+      <ul>
+        {market.map(market => (
+          <li key={market.id}>{market.name}</li>
+        ))}
+      </ul>
+      <Map></Map>
     </div>
   )
 }
