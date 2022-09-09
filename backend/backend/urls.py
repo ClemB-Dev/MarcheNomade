8874b8ee6f2128141a2ee87ae56b6deddb9bd329
Clémence <clemence.blachon@grafenit.com>
2022-09-09 17:32:52 +0200

    marche nomade app urls add

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('marche_nomade.api.urls'))
+    path('api/', include('marche_nomade.api.urls')),
+    path('marche_nomade/', include('marche_nomade.urls'))
 ]
