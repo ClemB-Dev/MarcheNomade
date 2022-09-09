3a6f8b332f3ab46562ccae670ec72d6a54ac4f80
Clémence <clemence.blachon@grafenit.com>
2022-09-09 17:37:13 +0200

    api functions: authentification

--- backend/marche_nomade/api/views.py
+++ backend/marche_nomade/api/views.py
@@ -1,10 +1,13 @@
 from rest_framework import generics
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view  # , permission_classes
+from rest_framework.permissions import AllowAny  # , IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
 from django.contrib.auth.models import User
-from .serializers import MyTokenObtainPairSerializer, RegisterSerializer
+from .serializers import (MyTokenObtainPairSerializer,
+                          RegisterSerializer,
+                          )
+
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
@@ -12,7 +15,7 @@ class MyTokenObtainPairView(TokenObtainPairView):
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
-    permissioin_classes = (AllowAny,)
+    permissions = (AllowAny,)
     serializer_class = RegisterSerializer
 
 
