d5ee3d7222745e7d1d9dc85050223e21e4cd98f8
Clémence <clemence.blachon@grafenit.com>
2022-09-09 17:43:18 +0200

    styling

--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -1,4 +1,5 @@
 import React, {useContext} from 'react'
+import { Link } from "react-router-dom";
 import AuthContext from '../context/AuthContext'
 
 const RegisterPage = () => {
@@ -6,23 +7,28 @@ const RegisterPage = () => {
 
   return (
     <div>
-      <form onSubmit={registerUser}>
-      <input
+      <form className='register-form' onSubmit={registerUser}>
+        <label htmlFor='first_name'>Firstname</label>
+        <input
             type='text'
             name='first_name'
             placeholder='Enter firstname'/>
+        <label htmlFor='last_name'>Lastname</label>
         <input
             type='text'
             name='last_name'
             placeholder='Enter lastname'/>
+        <label htmlFor='Email'>Email</label>
         <input
             type='text'
             name='email'
             placeholder='Enter email'/>                        
+        <label htmlFor='username'>Username</label>
         <input
             type='text'
             name='username'
             placeholder='Enter username'/>
+        <label htmlFor='password'>Password</label>
         <input
             id='password'
             type='password'
@@ -36,6 +42,7 @@ const RegisterPage = () => {
         <input
             type='submit'/>
       </form>
+      <Link className='already-registered-btn' to='/login'>Already have an account?</Link>
     </div>
   )
 }
