65ee491f9a9152b223cdaf3776e0bc09689e7ca5
Clémence <clemence.blachon@grafenit.com>
2022-09-12 23:13:27 +0200

    markets model updated: address format more detailed

--- backend/marche_nomade/models.py
+++ backend/marche_nomade/models.py
@@ -2,11 +2,18 @@ from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 from django.contrib.auth.models import User
 from multiselectfield import MultiSelectField
+from geopy.geocoders import Nominatim
 
 
 class Market(models.Model):
     name = models.CharField(max_length=100)
-    address = models.TextField()
+    number = models.IntegerField(blank=True, null=True)
+    address = models.CharField(max_length=100)
+    postcode = models.IntegerField()
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    latitude = models.CharField(max_length=200)
+    longitude = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
