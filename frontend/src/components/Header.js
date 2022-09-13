112121a00c27d29b3243c22f85ca7b721596714f
Clémence <clemence.blachon@grafenit.com>
2022-09-13 17:13:57 +0200

    styling

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,8 +2,9 @@ import React, {useContext} from 'react'
 import { Link } from 'react-router-dom';
 import AuthContext from '../context/AuthContext';
 import '../css/header.css'
-import UserImg from '../assets/icon-user.png'
-import HomeImg from '../assets/home.png'
+import LogInImg from '../assets/login.png'
+import LogOutImg from '../assets/logout.png'
+import HomeImg from '../assets/earth.png'
 
 
 const Header = () => {
@@ -12,8 +13,8 @@ const Header = () => {
     <div className='header' id='header'>
         <Link className='header-btn' to='/'><img className='header-icon' src={HomeImg}/></Link>
         {user 
-          ? (<Link className='header-btn' to='/login' onClick={logoutUser}>Logout</Link>)
-          : (<Link className='header-btn' to='/login'><img className='header-icon' src={UserImg}/></Link>)}
+          ? (<Link className='header-btn' to='/login' onClick={logoutUser}><img className='header-icon' src={LogOutImg}/></Link>)
+          : (<Link className='header-btn' to='/login'><img className='header-icon' src={LogInImg}/></Link>)}
         {user && <p>Hello {user.username}</p>}
     </div>
   )
